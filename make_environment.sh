1ef5372f910901faa1de737b7093215e46640b78
Alexey Chernikov <alexey.chernikov@gmail.com>
2009-11-03 23:49:46 +0500

    add .vimrc settings

--- make_environment.sh
+++ make_environment.sh
@@ -6,3 +6,4 @@ ln -f $path/bash/bashrc ~/.bashrc
 ln -f $path/ruby/irbrc ~/.irbrc
 ln -f $path/git/gitconfig ~/.gitconfig
 ln -f $path/autotest/autotest.rb ~/.autotest
+ln -f $path/vim/vimrc ~/.vimrc
