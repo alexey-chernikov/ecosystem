d39ac4d29dfd5cb697973db3bc7d74268bd66b3e
Alexey Chernikov <alexey.chernikov@gmail.com>
2010-05-03 17:11:02 +0600

    Mplayer and gemrc added

--- make_environment.sh
+++ make_environment.sh
@@ -3,7 +3,15 @@
 path=`dirname $0`
 
 ln -f $path/bash/bashrc ~/.bashrc
+
+ln -f $path/ruby/gemrc ~/.gemrc
 ln -f $path/ruby/irbrc ~/.irbrc
+
 ln -f $path/git/gitconfig ~/.gitconfig
+
 ln -f $path/autotest/autotest.rb ~/.autotest
+
 ln -f $path/vim/vimrc ~/.vimrc
+
+mkdir ~/.mplayer
+ln -f $path/mplayer/config ~/.mplayer/config
