6052559c8f6526f5afabfa96b037a8aa7a791b36
Alexey Chernikov <alexey.chernikov@gmail.com>
2010-05-21 15:38:14 +0600

    Added global git ignore

--- make_environment.sh
+++ make_environment.sh
@@ -5,5 +5,6 @@ path=`dirname $0`
 ln -f $path/bash/bashrc ~/.bashrc
 ln -f $path/ruby/irbrc ~/.irbrc
 ln -f $path/git/gitconfig ~/.gitconfig
+ln -f $path/git/gitignore ~/.gitignore
 ln -f $path/autotest/autotest.rb ~/.autotest
 ln -f $path/vim/vimrc ~/.vimrc
