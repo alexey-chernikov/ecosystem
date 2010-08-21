f84160c86c374a899bc25b357dde3caecf7a950f
Alexey Chernikov <alexey.chernikov@gmail.com>
2010-08-22 03:12:44 +0600

    Added gems-install file; added ssh/config file

--- make_environment.sh
+++ make_environment.sh
@@ -11,6 +11,7 @@ ln -f $path/git/gitconfig ~/.gitconfig
 ln -f $path/git/gitignore ~/.gitignore
 ln -f $path/autotest/autotest.rb ~/.autotest
 ln -f $path/vim/vimrc ~/.vimrc
+ln -f $path/ssh/config ~/.ssh/config
 
 mkdir ~/.mplayer 2>/dev/null
 ln -f $path/mplayer/config ~/.mplayer/config
