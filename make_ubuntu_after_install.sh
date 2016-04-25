dc2f87217f205f53aecf1e4d08d4ee40788152c6
Alexey Chernikov <alexey.chernikov@gmail.com>
2016-04-25 10:41:54 +0500

    Updated environment create

--- make_ubuntu_after_install.sh
+++ make_ubuntu_after_install.sh
@@ -1,9 +1,45 @@
-#!/bin/bash
-sudo apt-get install vim-gnome ubuntu-restricted-extras git ruby rubygems msttcorefonts ssmtp mdadm ctags nvidia-current nvidia-settings chromium-browser chromium-codecs-ffmpeg-nonfree chromium-browser-inspector htop keepassx curl
-# For ruby development
-sudo apt-get install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake
-# For ruby gems: mysql etc.
-sudo apt-get install libmysqlclient-dev libmagickwand-dev
+#sudo apt update
+#sudo apt upgrade
+#sudo add-apt-repository ppa:webupd8team/java
+#sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
+#sudo add-apt-repository ppa:wine/wine-builds
+#sudo apt update
 
-sudo apt-get autoremove
-cd && git clone git://github.com/malygos/vimfiles .vim && cd .vim && git submodule init && git submodule update
+sudo apt install vim-gtk3 nvidia-settings guake git ubuntu-restricted-extras steam shutter nautilus-dropbox flashplugin-installer xubuntu-community-wallpapers-xenial unity-tweak-tool libavcodec-extra htop nmap keepassx mc doublecmd-gtk ncdu guake-indicator gimp doublecmd-gtk p7zip synaptic wmctrl ssh
+
+# Developer packages
+sudo apt install mysql-server libmysqlclient-dev build-essential
+
+# Home packages
+sudo apt install kodi 
+
+# Not works yet: myrulib
+
+#Oracle Java 8
+#sudo apt install oracle-java8-installer
+
+#Ubuntu Make
+#sudo apt install ubuntu-make
+
+
+#Interesting stuff
+#sudo apt-get install bleachbit libdvd-pkg
+
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#sudo dpkg -i google-chrome-stable_current_amd64.deb
+#sudo apt-get install -f
+
+#Viber
+#cd ~/Загрузки/
+#rm -fr viber.deb*
+#wget http://download.cdn.viber.com/cdn/desktop/Linux/viber.deb
+#sudo dpkg -i viber.deb
+
+#git clone git://github.com/alexey-chernikov/ecosystem .ecosystem
+#cd .ecosystem
+#./make_environment.sh
+
+# Wine development
+#sudo dpkg --add-architecture i386
+#sudo apt install --install-recommends winehq-devel
+#sudo apt install winetricks
