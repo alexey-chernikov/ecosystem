e06928b42f4afff2409808864d2dd49369635a35
Alexey Chernikov <alexey.chernikov@gmail.com>
2016-06-30 16:00:53 +0500

    Add smbclient

--- make_ubuntu_after_install.sh
+++ make_ubuntu_after_install.sh
@@ -12,7 +12,7 @@
 #ln -sf $DATA_FOLDER/$dir ~/$dir
 #done
 
-sudo apt install vim-gtk3 nvidia-settings guake git ubuntu-restricted-extras steam shutter nautilus-dropbox flashplugin-installer xubuntu-community-wallpapers-xenial unity-tweak-tool libavcodec-extra htop nmap keepassx mc doublecmd-gtk ncdu guake-indicator gimp doublecmd-gtk p7zip synaptic wmctrl ssh nfs-common tmux pidgin system-config-lvm youtube-dl vlc unity-tweak-tool pv pavucontrol cifs-utils whois filezilla compizconfig-settings-manager rkhunter workrave python3-smbc
+sudo apt install vim-gtk3 nvidia-settings guake git ubuntu-restricted-extras steam shutter nautilus-dropbox flashplugin-installer xubuntu-community-wallpapers-xenial unity-tweak-tool libavcodec-extra htop nmap keepassx mc doublecmd-gtk ncdu guake-indicator gimp doublecmd-gtk p7zip synaptic wmctrl ssh nfs-common tmux pidgin system-config-lvm youtube-dl vlc unity-tweak-tool pv pavucontrol cifs-utils whois filezilla compizconfig-settings-manager rkhunter workrave python3-smbc smbclient
 
 sudo apt remove thunderbird postfix
 
