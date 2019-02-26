6cb97aba48028c4af4d89a65cec8cd7f404317c4
Alexey Chernikov <alexey.chernikov@gmail.com>
2019-02-26 20:13:36 +0500

    Reworked vim configuration

--- make_environment.sh
+++ make_environment.sh
@@ -2,7 +2,8 @@
 
 path=`pwd`
 
-git clone --depth 5 https://github.com/StevenBlack/hosts.git
+#git clone --depth 5 https://github.com/StevenBlack/hosts.git
+#cp hosts/hosts /etc/hosts
 
 ln -f $path/bash/bashrc ~/.bashrc
 ln -f $path/bash/bash_profile ~/.bash_profile
@@ -25,15 +26,18 @@ ln -f $path/tmux/tmux.conf ~/.tmux.conf
 ln -f $path/zsh/zshrc ~/.zshrc
 # Neovim configuration
 ln -sf $path/nvim ~/.config/nvim
-#if [ -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
-#  echo "Downloading vim-plug"
-#  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-#fi
 
+#Vim
+if [ ! -d ~/.vim ]; then
+    mkdir -p ~/.vim/bundle
+    mkdir -p ~/.vim/tmp
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+#Mplayer
 mkdir ~/.mplayer 2>/dev/null
 ln -f $path/mplayer/config ~/.mplayer/config
 
-cp hosts/hosts /etc/hosts
 
 # zsh steps
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
