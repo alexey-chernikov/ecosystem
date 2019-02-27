469d9a9d53689e2ab0ca0129eb3b31ba1b0d04e7
Alexey Chernikov <alexey.chernikov@gmail.com>
2019-02-27 11:08:56 +0500

    Vim init refactor

--- make_environment.sh
+++ make_environment.sh
@@ -29,10 +29,9 @@ ln -sf $path/nvim ~/.config/nvim
 
 #Vim
 if [ ! -d ~/.vim ]; then
-    mkdir -p ~/.vim/bundle
+    mkdir -p ~/.vim/autoload
     mkdir -p ~/.vim/tmp/undo
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    vim +PluginInstall +qall
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 #Mplayer
