cf6ffe8c68b28be63249f56c2b116b37e1863365
Alexey Chernikov <alexey.chernikov@gmail.com>
2019-02-28 14:58:15 +0500

    Symbolic links instead of hard links

--- make_environment.sh
+++ make_environment.sh
@@ -5,25 +5,25 @@ path=`pwd`
 #git clone --depth 5 https://github.com/StevenBlack/hosts.git
 #cp hosts/hosts /etc/hosts
 
-ln -f $path/bash/bashrc ~/.bashrc
-ln -f $path/bash/bash_profile ~/.bash_profile
-ln -f $path/bash/bash_aliases ~/.bash_aliases
-ln -f $path/bash/inputrc ~/.inputrc
+ln -sf $path/bash/bashrc ~/.bashrc
+ln -sf $path/bash/bash_profile ~/.bash_profile
+ln -sf $path/bash/bash_aliases ~/.bash_aliases
+ln -sf $path/bash/inputrc ~/.inputrc
 
-ln -f $path/ruby/gemrc ~/.gemrc
-ln -f $path/ruby/irbrc ~/.irbrc
-ln -f $path/ruby/rvmrc ~/.rvmrc
-ln -f $path/ruby/railsrc ~/.railsrc
+ln -sf $path/ruby/gemrc ~/.gemrc
+ln -sf $path/ruby/irbrc ~/.irbrc
+ln -sf $path/ruby/rvmrc ~/.rvmrc
+ln -sf $path/ruby/railsrc ~/.railsrc
 
 ln -sf $path/bin ~/.bin
 
-ln -f $path/git/gitconfig ~/.gitconfig
-ln -f $path/git/gitignore ~/.gitignore
-ln -f $path/autotest/autotest.rb ~/.autotest
-ln -f $path/vim/vimrc ~/.vimrc
-ln -f $path/ssh/config ~/.ssh/config
-ln -f $path/tmux/tmux.conf ~/.tmux.conf
-ln -f $path/zsh/zshrc ~/.zshrc
+ln -sf $path/git/gitconfig ~/.gitconfig
+ln -sf $path/git/gitignore ~/.gitignore
+ln -sf $path/autotest/autotest.rb ~/.autotest
+ln -sf $path/vim/vimrc ~/.vimrc
+ln -sf $path/ssh/config ~/.ssh/config
+ln -sf $path/tmux/tmux.conf ~/.tmux.conf
+ln -sf $path/zsh/zshrc ~/.zshrc
 # Neovim configuration
 ln -sf $path/nvim ~/.config/nvim
 
