015c9129e34f8113a811d083f7fa5a1c79dd40de
Alexey Chernikov <alexey.chernikov@gmail.com>
2019-03-01 13:15:41 +0500

    Add verbose message on oh-my-zsh install

--- make_environment.sh
+++ make_environment.sh
@@ -42,7 +42,8 @@ ln -f $path/mplayer/config ~/.mplayer/config
 
 # zsh steps
 if [ ! -d ~/.oh-my-zsh ]; then
-  git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-  git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    echo "Oh-my-zsh set up..."
+    git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
