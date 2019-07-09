98b74aea6c43527d41b673a3082c6ab5bb160c8b
Alexey Chernikov <alexey.chernikov@gmail.com>
2019-07-09 16:59:01 +0500

    Refactor to ubuntu 18.04

--- make_ubuntu_after_install.sh
+++ make_ubuntu_after_install.sh
@@ -1,27 +1,18 @@
-#sudo apt update
-#sudo apt upgrade
-#sudo add-apt-repository ppa:webupd8team/java
-#sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
-#sudo add-apt-repository ppa:wine/wine-builds
-#sudo add-apt-repository ppa:libretro/stable # Retroarch PPA
-#sudo add-apt-repository ppa:graphics-drivers/ppa
-#sudo apt update
-
-#DATA_FOLDER=/media/data
-#for dir in $(ls /media/data)
-#do
-#ln -sf $DATA_FOLDER/$dir ~/$dir
-#done
-
-sudo apt install vim-gtk3 nvidia-settings guake git ubuntu-restricted-extras steam shutter nautilus-dropbox flashplugin-installer xubuntu-community-wallpapers-xenial unity-tweak-tool libavcodec-extra htop nmap keepassx mc doublecmd-gtk ncdu guake-indicator gimp doublecmd-gtk p7zip synaptic wmctrl ssh nfs-common tmux pidgin system-config-lvm youtube-dl vlc unity-tweak-tool pv pavucontrol cifs-utils whois filezilla compizconfig-settings-manager rkhunter workrave python3-smbc smbclient ack-grep tmuxinator
-
-sudo apt remove thunderbird postfix
+sudo apt install vim-gnome git tig ubuntu-restricted-extras shutter nautilus-dropbox gnome-tweak-tool htop nmap mc doublecmd-gtk ncdu p7zip ssh nfs-common tmux mpv pv pavucontrol cifs-utils whois filezilla tmuxinator
+sudo snap install keepassxc
+snap install --channel=edge --classic rubymine
+snap install --classic android-studio
+snap install --classic --channel=edge intellij-idea-ultimate
 
 # Developer packages
-sudo apt install mysql-server libmysqlclient-dev build-essential mysql-workbench nodejs npm giggle giggle-personal-details-plugin giggle-terminal-view-plugin postgresql postgresql-contrib libpq-dev pgadmin3 virt-manager android-tools-adb pwgen meld mesa-utils openvpn ssmtp
+sudo apt install build-essential mysql-workbench giggle giggle-personal-details-plugin giggle-terminal-view-plugin virt-manager pwgen meld
+
+
+# Nvidia
+# sudo apt install nvidia-settings
 
 # System administrator packages
-sudo apt-get install qemu-kvm libvirt-bin
+# sudo apt-get install qemu-kvm libvirt-bin
 # Home packages
 #sudo apt install kodi deluge calibre
 
@@ -29,36 +20,9 @@ sudo apt-get install qemu-kvm libvirt-bin
 #gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 #curl -sSL https://get.rvm.io | bash -s stable
 
-#rvm install 2.3
-#rvm install 2.1
-#rvm install ree
-
-# Not works yet: myrulib
-
 #Oracle Java 8
 #sudo apt install oracle-java8-installer
-
-#Ubuntu Make
-#sudo apt install ubuntu-make
-
-#Interesting stuff
-#sudo apt-get install bleachbit libdvd-pkg
-
-#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-#sudo dpkg -i google-chrome-stable_current_amd64.deb
-#sudo apt-get install -f
-
-#Viber
-#cd ~/Загрузки/
-#rm -fr viber.deb*
-#wget http://download.cdn.viber.com/cdn/desktop/Linux/viber.deb
-#sudo dpkg -i viber.deb
 
 #git clone git://github.com/alexey-chernikov/ecosystem .ecosystem
 #cd .ecosystem
 #./make_environment.sh
-
-# Wine development
-#sudo dpkg --add-architecture i386
-#sudo apt install --install-recommends winehq-devel
-#sudo apt install winetricks
