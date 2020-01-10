6bf29031f2f97164e124f17cc5b92928ad532a8a
Alexey Chernikov <alexey.chernikov@gmail.com>
2020-01-11 01:37:37 +0500

    Add curl

--- make_ubuntu_after_install.sh
+++ make_ubuntu_after_install.sh
@@ -1,11 +1,10 @@
-sudo apt install vim-gnome git tig ubuntu-restricted-extras shutter nautilus-dropbox gnome-tweak-tool htop nmap mc doublecmd-gtk ncdu p7zip ssh nfs-common tmux mpv pv pavucontrol cifs-utils whois filezilla tmuxinator default-jre
-sudo snap install keepassxc
+sudo apt install curl vim-gnome git tig ubuntu-restricted-extras shutter nautilus-dropbox gnome-tweak-tool htop nmap mc doublecmd-gtk ncdu p7zip ssh nfs-common tmux mpv pv pavucontrol cifs-utils whois filezilla tmuxinator default-jre keepassxc
+#sudo snap install keepassxc
 snap install --channel=edge --classic rubymine
 snap install --classic android-studio
-snap install --classic --channel=edge intellij-idea-ultimate
 
 # Developer packages
-sudo apt install build-essential mysql-workbench giggle giggle-personal-details-plugin giggle-terminal-view-plugin virt-manager pwgen meld
+sudo apt install mysql-workbench giggle virt-manager pwgen meld
 
 
 # Nvidia
