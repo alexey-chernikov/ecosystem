3d0ae07ea87addc8bbbd2c1afe132faa9e3d89b5
Alexey Chernikov <alexey@chernikov.online>
2020-09-23 23:24:01 +0500

    Autoupdate powerlevel10k theme

--- make_environment.sh
+++ make_environment.sh
@@ -46,6 +46,6 @@ ln -f $path/mplayer/config ~/.mplayer/config
 if [ ! -d ~/.oh-my-zsh ]; then
     echo "Oh-my-zsh set up..."
     git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
