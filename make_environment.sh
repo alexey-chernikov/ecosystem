fef7a862171bb3413cff8345c4b892620776875b
Alexey Chernikov <alexey@chernikov.online>
2020-09-23 23:41:13 +0500

    Remove excessive theme download

--- make_environment.sh
+++ make_environment.sh
@@ -46,7 +46,6 @@ ln -f $path/mplayer/config ~/.mplayer/config
 if [ ! -d ~/.oh-my-zsh ]; then
     echo "Oh-my-zsh set up..."
     git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
 
