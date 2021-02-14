75d414e09b26cd54b6d8fed05c776a4a7e52ebbb
Alexey Chernikov <alexey@chernikov.online>
2021-02-14 19:41:11 +0500

    Install zsh-syntax-highlighting if not present

--- make_environment.sh
+++ make_environment.sh
@@ -56,6 +56,7 @@ if [ ! -d ~/.oh-my-zsh ]; then
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
 
+[ ! -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ] &&   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-nvm ] && git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
 
 if [ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]; then
