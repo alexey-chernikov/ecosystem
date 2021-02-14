d82b533a46f4a44fd82e9057dff37c77f840c237
Alexey Chernikov <alexey@chernikov.online>
2021-02-14 19:44:42 +0500

    Remove excessive lines

--- make_environment.sh
+++ make_environment.sh
@@ -53,7 +53,6 @@ ln -f $path/mplayer/config ~/.mplayer/config
 if [ ! -d ~/.oh-my-zsh ]; then
     echo "Oh-my-zsh set up..."
     git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
 
 [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ] &&   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
