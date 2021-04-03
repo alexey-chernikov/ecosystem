21936dc3cad2866561fbd460e1b8e148edfb15d1
Alexey Chernikov <alexey@chernikov.online>
2021-04-03 22:47:18 +0500

    Install docker

--- make_ubuntu_after_install.sh
+++ make_ubuntu_after_install.sh
@@ -15,6 +15,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
 # System administrator packages
 # sudo apt-get install qemu-kvm libvirt-bin
 # Home packages
