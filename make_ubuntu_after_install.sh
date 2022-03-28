73f5707cc098ab2ce6f623c1a359f6efd0ddc7d5
Alexey Chernikov <alexey@chernikov.online>
2022-03-28 14:13:56 +0400

    Change lib name

--- make_ubuntu_after_install.sh
+++ make_ubuntu_after_install.sh
@@ -9,7 +9,7 @@ sudo apt install giggle pwgen meld mysql-client postgresql-client libmysqlclient
 #mysql-workbench
 
 # Ruby dependencies
-sudo apt install libreadline-dev libsqlite3-dev libmagickwand-dev libxlst-dev
+sudo apt install libreadline-dev libsqlite3-dev libmagickwand-dev libxslt1-dev
 
 IFS=$'\n'       # make newlines the only separator
 for snappackage in $(cat snaps)
